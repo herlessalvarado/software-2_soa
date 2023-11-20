40c63449a97cbe9306bbaa52e23fbafa0b4fb612
Herless Alvarado <herlessalvarado@gmail.com>
2023-11-19 22:12:08 -0500

    api gateway working

---
+++ api-gateway/app.js
@@ -0,0 +1,41 @@
+const e = require("express");
+const express = require("express");
+const { createProxyMiddleware } = require("http-proxy-middleware");
+const axios = require("axios").default;
+
+const app = express();
+const PORT = 3000;
+
+const authProxy = createProxyMiddleware({
+  target: "http://localhost:3001",
+  changeOrigin: true,
+});
+
+const weatherProxy = createProxyMiddleware({
+  target: "http://localhost:3002",
+  changeOrigin: true,
+});
+
+app.use("/auth/*", (req, res, next) => {
+  authProxy(req, res, next);
+});
+
+app.use("/weather/*", async (req, res, next) => {
+  const token = req.headers.authorization;
+  const isValid = await axios.get("http://localhost:3000/auth/protected", {
+    headers: { authorization: token },
+  });
+  if (isValid.status === 200) {
+    weatherProxy(req, res, next);
+  } else {
+    res.status(isValid.statusCode).json(isValid.body);
+  }
+});
+
+app.use("/", (req, res) => {
+  res.status(404).json({ message: "Not Found" });
+});
+
+app.listen(PORT, () => {
+  console.log(`API Gateway listening on port ${PORT}`);
+});
